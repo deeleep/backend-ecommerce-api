9ac27371be7aa2f89779b731a81fdbdfdb3bf97c
Dilip Mehata <deeleepkushwaha@gmail.com>
2024-06-07 19:19:07 +0900

    Add notification service

---
+++ services/notification/src/test/java/com/dmeh/ecommerce/NotificationApplicationTests.java
@@ -0,0 +1,14 @@
+package com.dmeh.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+// Ignore build and .DS_Store files
